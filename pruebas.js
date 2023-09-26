a5a43ce70918bb5f992c061ef40a631363e8f814
Daviruchi <alejandrocardona710@gmail.com>
2023-09-26 11:18:01 -0500

    Ejercicios terminados hasta el modulo 8

--- pruebas.js
+++ pruebas.js
@@ -1,35 +1,14 @@
-class Persona {
-   // Crea el constructor de la clase "Persona".
-   // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
-   // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
-   // sus valores.
-   constructor(nombre, apellido, edad, domicilio){
-      this.nombre = nombre;
-      this.apellido = apellido;
-      this.edad = edad;
-      this.domicilio = domicilio;
-   }
-   detalle(){
-      let persona = {
-         nombre: this.nombre,
-         apellido: this.apellido,
-         edad: this.edad,
-         domicilio: this.domicilio,
-      }
-      return persona;
-   }    
-   
-}
-
-
-
-function crearInstanciaPersona(nombreInstancia, nombre, apellido, edad, domicilio) {
-   // En este ejercicio debes crear una instancia de la clase construida en el ejercicio anterior.
-   // Recibirás las propiedades por parámetro.
-   // Retornar la instancia creada.
+function mayuscula(nombre) {
+   // Esta función recibe un nombre (string).
+   // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
+   // [Ejemplo]: "mario" ----> "Mario".
    // Tu código:
-   nombreInstancia = new Persona (nombre,apellido,edad,domicilio);
-   return nombreInstancia;
+   stringToArray = nombre.split("");
+   console.log(stringToArray);
+   stringToArray[0] = stringToArray[0].toUpperCase();
+   //console.log(stringToArray[0]);
+   return stringToArray.join("");
+
 }
 
-console.log(crearInstanciaPersona("instancia1","Alejo", "Cardona", 37,"Calle falsa 123"));
+console.log(mayuscula("alejo"));
